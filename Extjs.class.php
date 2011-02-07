016ef2cc6f1030d6bf119ee6dbb4720abf037cc3
gerhard oettl <gerhard.oettl@asinoe.at>
2011-02-07 14:38:22 +0100

    introduce extjs helper class

---
+++ Extjs.class.php
@@ -0,0 +1,24 @@
+<?PHP
+/*
+#LICENSE BEGIN
+#LICENSE END
+*/
+
+
+/**
+* Helper routines for extjs.
+*/
+class Extjs {
+
+  /**
+  * Create failure message.
+  */
+  public static function failureMsg($msg) {
+    $msg = str_replace("'", "\'", $msg);
+    $msg = str_replace('"', '\"', $msg);
+    return "{ success: false, msg: '" . $msg . "' }";
+  }  // failure msg json object
+
+
+}  // end of class
+?>
