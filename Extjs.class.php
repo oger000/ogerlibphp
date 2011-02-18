8d08bb8651d303d7a82d65cd93556f286d6724d9
oger000 <gerhard.oettl@ogersoft.at>
2011-02-18 18:26:22 +0100

    extend exjs messages

--- Extjs.class.php
+++ Extjs.class.php
@@ -11,13 +11,56 @@
 class Extjs {
 
   /**
-  * Create failure message.
+  * Create an extjs json message with success property.
+  * @msg: message text
+  * @success: success value. Defaults to true.
   */
-  public static function failureMsg($msg) {
-    $msg = str_replace("'", "\'", $msg);
-    //$msg = str_replace('"', '\"', $msg);
-    return "{ success: false, msg: '" . $msg . "' }";
-  }  // failure msg json object
+  public static function message($msg, $success = true) {
+
+    // escape string delimiter (we hope that json_encode does this for us)
+    //$msg = str_replace("'", "\'", $msg);
+    ////$msg = str_replace('"', '\"', $msg);
+
+    return json_encode(array('success' => (boolean)$success, 'msg' => $msg));
+
+  }  // create a extjs json message
+
+
+
+  /**
+  * Create a error message.
+  * Is an alias for an unsuccess message.
+  */
+  public static function errorMsg($msg) {
+    return self::unsuccessMsg($msg);
+  }
+
+
+
+  /**
+  * Create an unsuccess message.
+  */
+  public static function unsuccessMsg($msg) {
+    return self::message($msg, false);
+  }
+
+
+
+  /**
+  * Create a success message.
+  */
+  public static function successMsg($msg) {
+    return self::message($msg, true);
+  }
+
+
+
+  /**
+  * Create a success object (without message).
+  */
+  public static function success() {
+    return '{ success: true }';
+  }
 
 
 }  // end of class
