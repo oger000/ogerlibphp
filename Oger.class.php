1a47ebb29c73eee46224214cdbd228a80c3e07b6
oger000 <gerhard.oettl@ogersoft.at>
2013-04-11 12:17:22 +0200

    duplicate assoz array check into Oger for ogerlibphp12 functions

--- Oger.class.php
+++ Oger.class.php
@@ -52,6 +52,31 @@ class Oger {
 
 
 
+  /**
+   * BACKPORT from Oger12 for ogerlibphp12 calls
+  * Check if array is associative.
+  * An array is assiciative if it has non numeric keys.<BR>
+  * <em>ATTENTION:</em> Associative arrays with only numeric keys are
+  * treated as NOT associative!!!! This is a general problem
+  * also in PHP internal-functions like array_merge, etc.
+  * @param $array  Array to be checked.
+  * @return True if it is an associative array. False otherwise.
+  */
+  public static function isAssocArray($array) {
+    if (!is_array($array)) {
+      return false;
+    }
+    foreach ($array as $key => $value) {
+      if (!is_numeric($key)) {
+        return true;
+      }
+    }
+    return false;
+  }  // eo assoc check
+
+
+
+
 }  // eo class
 
 ?>
